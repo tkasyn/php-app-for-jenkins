8fc6ec588c0e067111f99dc20a49e5bbf3240b84
Taras K <78083579+tkasyn@users.noreply.github.com>
2022-01-21 14:53:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ else
         <h1>Congratulations DevOps Engineer!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
-        <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
+        <p>This environment is launched with Elastic Beanstalk PHP Platform by Taras</p>
     </section>
 
     <section class="instructions">
